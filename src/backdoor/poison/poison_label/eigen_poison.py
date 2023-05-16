fix covarriance calculation

src/backdoor/poison/poison_label/eigen_poison.py
@@ -15,12 +15,11 @@ device = torch.device("cuda:0")
 current_eigenvector = -1
 
 def PCA(X: torch.Tensor) -> torch.Tensor:
-    X_centered = mean_center(X)
-    cov_X = torch.linalg.matmul(X_centered.mH, X_centered)
-    cov_X = (1.0/X.shape[0])*cov_X
+    #X_centered = mean_center(X)
+    #cov_X = torch.mm(X_centered.t(), X_centered) / X_centered.size(0)
+    cov_X = torch.cov(X.t())
     L, V = torch.linalg.eigh(cov_X)
-    return L, V
-
+    return L,V
 
 """
 Convert a matrix from complex type to real
@@ -45,19 +44,13 @@ Takes as input a dataset and model.
 """
 
 
-def eigen_decompose(dataset, model_for_latent_space, check_cache=False):
-    if check_cache is True and os.path.exists("./cache/latent_space.pt") \
-            and os.path.exists("./cache/eigen_basis.pt")\
-            and os.path.exists("./cache/latent_space_in_basis.pt")\
-            and os.path.exists("./cache/label_list.pt"):
-
+def eigen_decompose(dataset, model_for_latent_space, check_cache=True):
+    if check_cache is True and os.path.exists("./cache/latent_space.pt") and os.path.exists("./cache/label_list.pt"):
         print("Found decomposition in cache")
         latent_space = torch.load("./cache/latent_space.pt")
-        eigen_basis = torch.load("./cache/eigen_basis.pt")
-        vectors_in_basis = torch.load("./cache/latent_space_in_basis.pt")
         label_list = torch.load("./cache/label_list.pt")
-        return latent_space, vectors_in_basis, eigen_basis, label_list
     else:
+        print("creating decomp")
         latent_space, label_list = generate_latent_space(dataset, model_for_latent_space)
 
     eigen_values, eigen_basis = PCA(latent_space)
@@ -67,13 +60,14 @@ def eigen_decompose(dataset, model_for_latent_space, check_cache=False):
     # rewrite as basis
     vectors_in_basis = write_vectors_as_basis(latent_space, basis_inverse)
 
+    print(eigen_values)
     # cache everything
     torch.save(latent_space, "./cache/latent_space.pt")
     torch.save(eigen_basis, "./cache/eigen_basis.pt")
     torch.save(vectors_in_basis, "./cache/latent_space_in_basis.pt")
     torch.save(label_list, "./cache/label_list.pt")
-
-    return latent_space, vectors_in_basis, eigen_basis,label_list
+    print("latent results have been cached")
+    return latent_space, vectors_in_basis, eigen_basis, label_list
 
 
 def write_vectors_as_basis(latent_space, basis_inverse):
@@ -96,18 +90,15 @@ def write_vectors_as_basis(latent_space, basis_inverse):
 def generate_latent_space(dataset, latent_generator_model):
     latent_space = []
 
-    dl = DataLoader(dataset, batch_size=256, shuffle=True, num_workers=16)  # change batch size
+    dl = DataLoader(dataset, batch_size=256, shuffle=False, num_workers=16)  # change batch size
     pbar = tqdm(dl)
     label_list = []
-    i = 0 # early stopping for testing
 
     for d, label in pbar:
         latent_generator_model(d.to(device))
         t = latent_generator_model.get_features().detach()
         latent_space.append(t)
         label_list.append(label)
-        i = i+1
-        if i == 66: break
 
     result = torch.cat(latent_space, dim=0).detach()
     label_list = torch.cat(label_list).detach()
@@ -126,6 +117,9 @@ class Eigenpoison(Backdoor):
     def embed(self, x: torch.Tensor, y: torch.Tensor, **kwargs) -> Tuple:
         return None;
 
+    """
+    number of poison examples = #vectors_to_poison * poisons_per_vector * 2 (both directions)
+    """
     def choose_poisoning_targets(self, class_to_idx: dict, dataset: torch.Tensor) -> List[int]:
         num_samples = self.backdoor_args.poison_num
         return None
@@ -180,8 +174,6 @@ def main():
     class_means = compute_class_means(latent_space_in_basis, label_list, 1000)
 
     total_order = create_total_order_for_each_eigenvector(class_means, basis)
-
-    print(total_order[1])
     print("done")
     # clustering of latent space
 