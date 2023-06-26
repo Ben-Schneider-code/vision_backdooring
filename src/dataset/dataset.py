fix normalization in dataset

src/dataset/dataset.py
@@ -218,7 +218,7 @@ class Dataset(torch.utils.data.Dataset, ABC):
             dl = DataLoader(self.subset([self.idx.index(ti) for ti in target_idx]).without_normalization(),
                             batch_size=1 if self.dataset_args.singular_embed else self.env_args.batch_size,
                             drop_last=False,
-                            num_workers=0, shuffle=False)
+                            num_workers=self.env_args.num_workers, shuffle=False)
             ctr = 0
             item_indices = target_idx
             target_idx = target_idx if self.train else [-idx for idx in target_idx]
@@ -238,12 +238,10 @@ class Dataset(torch.utils.data.Dataset, ABC):
         x, y0 = self.dataset[index]
         y = y0
         x = self.transform(x)  # transform without normalize
-        fetched = False
         for backdoor in self.idx_to_backdoor.setdefault(index, []):
             if backdoor.requires_preparation() and not self.disable_fetching:
                 try:
                     x, y = backdoor.fetch(index if self.train else -index)  # fetch precomputed results
-                    fetched = True  # keep a flag to prevent normalizing twice
                 except:
                     print(f"Tried fetching index='{index}' with backdoor, but could not. "
                           f"No backdoor will be embedded.")
@@ -260,7 +258,7 @@ class Dataset(torch.utils.data.Dataset, ABC):
 
         if isinstance(y_out, (int, float)):
             y_out = torch.tensor(y_out)
-        if self.dataset_args.normalize and not fetched:
+        if self.dataset_args.normalize:
             return self.normalize(x), y_out
         return x, y_out
 