4ad8e35c82cc8752d98d084488aaa12881ed3e72
ben-schneider-code <schnei19@myumanitoba.ca>
2023-11-20 17:09:03 -0600

    add STRIP defense

--- examples/data_defense.py
+++ examples/data_defense.py
@@ -1,6 +1,10 @@
 from dataclasses import asdict
 
+import numpy as np
+import torch
 import transformers
+from torch.utils.data import DataLoader
+
 from src.arguments.backdoored_model_args import BackdooredModelArgs
 from src.arguments.config_args import ConfigArgs
 from src.arguments.dataset_args import DatasetArgs
@@ -12,14 +16,14 @@ from src.arguments.outdir_args import OutdirArgs
 from src.backdoor.backdoor import Backdoor
 from src.dataset.dataset import Dataset
 from src.dataset.dataset_factory import DatasetFactory
-from src.defenses.defense import Defense
-from src.defenses.defense_factory import DefenseFactory
 from src.model.model import Model
-from src.observers.observer_factory import ObserverFactory
-from src.utils.defense_util import plot_defense
-from src.utils.distributed_validation import poison_validation_ds
-from src.utils.special_print import print_highlighted, print_dict_highlighted
+from src.defenses.data.STRIP import run
+
 import os
+
+from src.utils.distributed_validation import poison_validation_ds
+from src.utils.special_images import plot_images
+
 
 def main(config_args: ConfigArgs):
     if config_args.exists():
@@ -39,54 +43,47 @@ def main(config_args: ConfigArgs):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(env_args.gpus[0])
 
 
+    num_samples = 1000
 
     model, backdoor = backdoored_model_args.unpickle(model_args, env_args)
     model: Model = model.cuda().eval()
-
     backdoor: Backdoor = backdoor
-
     # Compatibility hack
     backdoor.in_classes = None
-
-
-    wandb_config: dict = {
-        'project_name': out_args.wandb_project,
-        'config': asdict(backdoor.backdoor_args) | asdict(defense_args) | asdict(model_args) | asdict(dataset_args) | asdict(out_args),
-        'dir': '~',
-        'name' : defense_args.def_name
-    }
-
-    ds_train: Dataset = DatasetFactory.from_dataset_args(dataset_args, train=True)
-    ds_val: Dataset = DatasetFactory.from_dataset_args(dataset_args, train=False)
+    ds_clean: Dataset = DatasetFactory.from_dataset_args(dataset_args, train=False)
+    dl = DataLoader(ds_clean, num_workers=0, shuffle=True, batch_size=num_samples)
+    _, batch = next(enumerate(dl))
+    batch = (batch[0], batch[1])
 
     ds_poisoned: Dataset = DatasetFactory.from_dataset_args(dataset_args, train=False)
     ds_poisoned = poison_validation_ds(ds_poisoned, backdoor, len(ds_poisoned))
+    dl_poisoned = DataLoader(ds_poisoned, num_workers=0, shuffle=True, batch_size=num_samples)
+    dl_clean = DataLoader(ds_clean, num_workers=0, shuffle=True, batch_size=num_samples)
 
-    ds_poisoned = ds_poisoned.random_subset(10_000)
-    ds_val = ds_val.random_subset(10_000)
+    data_defense = run(model, batch, num_samples)
 
-    defense: Defense = DefenseFactory.from_defense_args(defense_args, env_args=env_args, wandb_config=wandb_config)
-    observers = ObserverFactory.from_observer_args(observer_args, env_args=env_args)
-    defense.add_observers(observers)
+    _, clean_batch = next(enumerate(dl_clean))
+    _, poisoned_batch = next(enumerate(dl_poisoned))
+    clean_batch = clean_batch[0].numpy()
+    poisoned_batch = poisoned_batch[0].numpy()
 
-    print_dict_highlighted(asdict(backdoor.backdoor_args))
+    #plot_images(clean_batch[0])
+    #plot_images(poisoned_batch[0])
 
-    print_highlighted(defense.defense_args.def_name)
-    clean_model = defense.apply(model, ds_train, backdoor=backdoor, ds_test=ds_val, ds_poison_asr=ds_poisoned)
+    pred_clean = data_defense.get_predictions(clean_batch)
+    pred_poisoned = data_defense.get_predictions(poisoned_batch)
 
-    print_highlighted("FINAL STATISTICS")
-    print_dict_highlighted({
-        'ASR': clean_model.evaluate(ds_poisoned, verbose=False),
-        'CDA': clean_model.evaluate(ds_val, verbose=False)
-    })
+    print("\n\n\n\nCLEAN")
+    print("poisoned is: " + str(pred_clean.count(1)))
+    print("clean is: " + str(pred_clean.count(0)))
 
-    plot_defense(defense.metric)
-
+    print("\n\n\n\nPOISON")
+    print("poisoned is: " + str(pred_poisoned.count(1)))
+    print("clean is: " + str(pred_poisoned.count(0)))
 
 def parse_args():
     parser = transformers.HfArgumentParser(ConfigArgs)
     return parser.parse_args_into_dataclasses()
-
 
 
 if __name__ == "__main__":
